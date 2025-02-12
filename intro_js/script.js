a1988871027fa70da9c255c86babf7d3dd921c23
Rodrigo Bossini <professorbossini@gmail.com>
2025-02-12 17:26:44 -0300

    learn(ap01-js): encerra a secao 1.1
    - vimos var, let e const

---
+++ intro_js/script.js
@@ -0,0 +1,36 @@
+//pq não usar o var
+// var idade = 18
+// console.log('Oi, ' + nome)
+// //içamento (hoist)
+// if(idade >= 18){
+//     let nome = 'João'
+//     console.log('Parabéns, ' + nome + '. Você pode dirigir')
+// }
+// console.log("Até mais, " + nome)
+// var linguagem = "Javascript"
+// console.log ("Aprendendo " + linguagem)
+// var linguagem = "Java"
+// console.log("Aprendendo " + linguagem)
+//declarando variáveis
+// var a = 2
+// console.log(a)
+// a = 3
+// console.log(a)
+
+// let b
+// console.log(b)
+// const c
+// c = 2
+// let a = 2
+// console.log(a)
+// a = 3
+// console.log(a)
+
+
+//declarando constantes
+// const idade = 27
+//Java: String nome = "José";
+// const nome = 'José'
+// console.log(nome)
+// nome = 'Pedro'
+// console.log(nome)
