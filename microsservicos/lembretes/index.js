15325dec07a22febd87f1a17afba4724df49f504
Rodrigo Bossini <professorbossini@gmail.com>
2025-04-02 18:05:16 -0300

    learn(ap01-js): encerra o codigo 4.3.1

---
+++ microsservicos/lembretes/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+//localhost:4000/lembretes
+
+//GET /lembretes () => {} (endpoint)
+app.get('/lembretes', (req, res) => {
+
+})
+
+//POST /lembretes () => {} (endpoint)
+app.post('/lembretes', (req, res) => {})
